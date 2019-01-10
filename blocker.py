a586784306c7a4fdebc68e785f4ab493ac89afc9
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-01-10 13:35:56 -0600

    create gitignore and set up structure

--- blocker.py
+++ blocker.py
@@ -3,12 +3,16 @@ Website Blocker for certain times in the day
 '''
 
 import time
+from datetime import datetime as dt 
 
 hosts_path = 'C:\Windows\System32\drivers\etc\hosts'
 redirect = '127.0.0.1'
 websites =['www.facebook.com', 'facebook.com', 'www.google.com', 'google.com']
 
 while True:
-    print(1)
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print('Print Working hours')
+    else:
+        print('Fun hours')
     time.sleep(5)
     
