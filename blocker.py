e4cd62bf9596b2a812242f47501bf497f482a71a
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-01-10 13:50:47 -0600

    Prepares blocker structure for when in time

--- blocker.py
+++ blocker.py
@@ -6,12 +6,26 @@ import time
 from datetime import datetime as dt 
 
 hosts_path = 'C:\Windows\System32\drivers\etc\hosts'
+hosts_temp = 'hosts'
 redirect = '127.0.0.1'
 websites =['www.facebook.com', 'facebook.com', 'www.google.com', 'google.com']
 
+'''
+Adding sites to hosts file that will block them from being accessed
+'''
 while True:
+    #Compares the current time to the time that you wish to have the sites blocked
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
-        print('Print Working hours')
+        #opens the host file and check to see if the sites are already printed in the file
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            #For each site in the list, if the site is in the file pass otherwise write the site to the list
+            for site in websites:
+                if site in content:
+                    pass
+                else:
+                    file.write(redirect + " " + site + "\n")
+    #When not in the time frame remove the sites from the file
     else:
         print('Fun hours')
     time.sleep(5)
