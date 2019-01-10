7407fea5f3c378e80746450d8d662f1fc58a8bb9
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-01-10 13:18:29 -0600

    Initial Commit

---
+++ blocker.py
@@ -0,0 +1,14 @@
+'''
+Website Blocker for certain times in the day
+'''
+
+import time
+
+hosts_path = 'C:\Windows\System32\drivers\etc\hosts'
+redirect = '127.0.0.1'
+websites =['www.facebook.com', 'facebook.com', 'www.google.com', 'google.com']
+
+while True:
+    print(1)
+    time.sleep(5)
+    
